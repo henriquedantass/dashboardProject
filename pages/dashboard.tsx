85cbf5686a0141f3d5f789b55b50858d58cba0df
henrique dantas <paulo.henrique@sapienstec.com>
2021-07-14 11:45:49 -0300

    sidebar component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import {Flex} from '@chakra-ui/react'
+import React from 'react';
 import { Header } from '../src/components/Header';
+import { Sidebar } from '../src/components/Sidebar';
 
 export default function Dashboard(){
   return (
-    <Header/>
+    <Flex flexDir='column' h='100vh'> 
+      <Header/>
+      <Flex w='100%' maxWidth={1480} mx='auto' px='6'>
+        <Sidebar/>
+      </Flex>
+    </Flex>
   )
 }
